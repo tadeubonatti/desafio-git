3baf655442f096401bc71f0fbfc5058f6091da08
tadeubonatti <tadeu.bonatti@gmail.com>
2013-06-24 20:37:30 -0300

    Revert do Formatado Codigo

--- src/main/java/com/contaazul/printer/ArrayPrinter.java
+++ src/main/java/com/contaazul/printer/ArrayPrinter.java
@@ -6,7 +6,6 @@ public class ArrayPrinter {
 	{
 		for (int i = 0; i < input.length; i++) {
 			Object element = input[i];
-
 			System.out.printf( "%s ", new Object[] { element } );
 		}
 		System.out.println();
